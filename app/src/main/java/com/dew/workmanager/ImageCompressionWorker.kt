38dc5c52205dbdfce3199ebefbdbefccbbe9de85
DEW <myselfdew01@gmail.com>
2023-10-28 09:54:32 +0530

    Image compression worker class created

---
+++ app/src/main/java/com/dew/workmanager/ImageCompressionWorker.kt
@@ -0,0 +1,56 @@
+package com.dew.workmanager
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import kotlin.math.roundToInt
+
+class ImageCompressionWorker(private val appContext: Context, private val param: WorkerParameters) :
+    CoroutineWorker(appContext, param) {
+
+    override suspend fun doWork(): Result {
+
+        return withContext(Dispatchers.IO) {
+
+            val url = param.inputData.getString(KEY_IMG_URL)
+            val byteArray = appContext.contentResolver.openInputStream(Uri.parse(url))?.use {
+                it.readBytes()
+            } ?: return@withContext Result.failure()
+
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+
+            var outputByteArray: ByteArray
+            var quality = 100
+            do {
+                val outputStream = ByteArrayOutputStream()
+                outputStream.use { outputStream ->
+                    bitmap.compress(Bitmap.CompressFormat.PNG, quality, outputStream)
+                    outputByteArray = outputStream.toByteArray()
+                    quality -= (quality * 0.1).roundToInt()
+                }
+            } while (outputByteArray.size > 1024 * 10 && quality > 5)
+
+            val file = File(appContext.cacheDir, "${param.id}.png")
+            file.writeBytes(outputByteArray)
+
+            return@withContext Result.success(
+                workDataOf(
+                    KEY_OUTPUT_IMG_URL to file.absolutePath
+                )
+            )
+        }
+    }
+
+    companion object {
+        const val KEY_IMG_URL = "KEY_IMG_URL"
+        const val KEY_OUTPUT_IMG_URL = "KEY_OUTPUT_IMG_URL"
+    }
+}
